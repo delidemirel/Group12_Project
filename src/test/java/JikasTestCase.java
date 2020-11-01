8eb9e6886b3d2a69136cb10c27e12c6b3901beb4
kulmurzaeva84 <kjika84@gmail.com>
2020-11-01 14:36:01 -0600

    Updating Jika's test case

--- src/test/java/JikasTestCase.java
+++ src/test/java/JikasTestCase.java
@@ -1,5 +1,7 @@
 public class JikasTestCase {
     public static void main(String[] args) {
         System.out.println("Let me make some changes to the code");
+
+        // updating the code
     }
 }
