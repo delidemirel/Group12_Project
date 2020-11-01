1c2edb763899a96656c686a32db2fd569d4b4f8a
Nurbiye <nurbiye720@gmail.com>
2020-11-01 15:26:59 -0600

    Nur's update

--- src/test/java/LastGitHubClass.java
+++ src/test/java/LastGitHubClass.java
@@ -1,4 +1,7 @@
 public class LastGitHubClass {
     //well, almost there
+    public static void main(String[] args) {
+        System.out.println("I have learned a lot about Git and GitHub");
+    }
 
 }
