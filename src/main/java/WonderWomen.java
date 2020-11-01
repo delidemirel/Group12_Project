d254a57780352597d9be7f601733b64cf2f1d7b4
ErnieB20 <ernie312us@gmail.com>
2020-11-01 14:34:35 -0600

    Added changes to WonderWoman

--- src/main/java/WonderWomen.java
+++ src/main/java/WonderWomen.java
@@ -2,5 +2,6 @@ public class WonderWomen {
     public static void main(String[] args) {
 
         System.out.println("I am the Wonder Women");
+        // I want to be an associate for Wonder Woman
     }
 }
