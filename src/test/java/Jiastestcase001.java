0f9aed8e68fb21ed1602ce59ff1ae3e69251ea80
kulmurzaeva84 <kjika84@gmail.com>
2020-11-01 14:40:15 -0600

    Updated Jika's test case 001 on feature branch

--- src/test/java/Jiastestcase001.java
+++ src/test/java/Jiastestcase001.java
@@ -1,5 +1,7 @@
 public class Jiastestcase001 {
     public static void main(String[] args) {
         System.out.println("That is a message in Jika's branch");
+
+        System.out.println("Updating my message in a feature branch");
     }
 }
